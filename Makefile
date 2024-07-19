1ad7478a922b14c499c0dd2318d11e62e079721a
garsaud <cyril.garsaud@jurihub.fr>
2024-07-19 22:45:44 +0200

    using X11 to move mouse in infinite loop

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS=-Wall -Os -fno-ident -fno-asynchronous-unwind-tables -ffreestanding
+CFLAGS=-Wall -Os -fno-ident -fno-asynchronous-unwind-tables -ffreestanding -L/usr/X11R6/lib -L/usr/local/lib -lX11 -lXtst -lXinerama -lxkbcommon -I/usr/X11R6/include -I/usr/local/include
 CC=gcc ${CFLAGS}
 INSTALL_DIR=/usr/local/bin
 
--- main.c
+++ main.c
@@ -1,4 +1,18 @@
+#include <unistd.h>
+#include <X11/extensions/XTest.h>
+
 int main(void)
 {
+    char *display_name = XDisplayName(NULL);
+    Display *display = XOpenDisplay(display_name);
+
+    for (;;) {
+        XTestFakeRelativeMotionEvent(display, 1, 1, 0);
+        XFlush(display);
+        XTestFakeRelativeMotionEvent(display, -1, -1, 0);
+        XFlush(display);
+        sleep(10);
+    }
+
     return 0;
 }
