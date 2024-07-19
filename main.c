f564fe620a0ef47d9200194a1d4d7d3c522a16df
garsaud <cyril.garsaud@jurihub.fr>
2024-07-19 23:56:47 +0200

    loop until SIGINT

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
+#include <signal.h>
 #include <unistd.h>
 #include <X11/extensions/XTest.h>
+
+volatile sig_atomic_t stop;
+
+void signal_handler(int signum)
+{
+    stop = 1;
+}
 
 int main(void)
 {
     char *display_name = XDisplayName(NULL);
     Display *display = XOpenDisplay(display_name);
 
-    for (;;) {
+    signal(SIGINT, signal_handler);
+
+    while (!stop) {
         XTestFakeRelativeMotionEvent(display, 1, 1, 0);
         XFlush(display);
         XTestFakeRelativeMotionEvent(display, -1, -1, 0);
